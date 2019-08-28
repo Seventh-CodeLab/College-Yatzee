33411102b654a63b0a01480f995aff8bdd394726
Krister <37347340+SeventhDisaster@users.noreply.github.com>
2019-08-28 16:25:31 +0200

    Initial Commit

---
+++ src/test/java/no/kristiania/roman/YatzeeTest.java
@@ -0,0 +1,12 @@
+package no.kristiania.roman;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class YatzeeTest {
+    @Test
+    void shouldReturn4(){
+        assertEquals(4,2+2);
+    }
+}
