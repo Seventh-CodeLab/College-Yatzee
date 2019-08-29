d229c77424d047c385f8e17327bc9d317a8d80d5
Krister <SeventhDisaster@outlook.com>
2019-08-29 20:35:51 +0200

    Large refactor: Increased readability, cleared need for decrements and increments when comparing counter indexes. Changed several variable names to give more info

--- src/main/java/no/kristiania/yahtzee/Yahtzee.java
+++ src/main/java/no/kristiania/yahtzee/Yahtzee.java
@@ -4,24 +4,24 @@ package no.kristiania.yahtzee;
 
 class Yahtzee {
     static int getScore(Category category, int[] rolls){
-        int[] count = diceCounter(rolls); //Keeps track of how many of each dice are thrown. Die value = index + 1
+        int[] dieCount = diceCounter(rolls); //Tracks roll results: Index 1 = Dice with 1's, etc.
 
         switch (category){
-            case ONES: return count[0];
-            case TWOS: return count[1]*2;
-            case THREES: return count[2]*3;
-            case FOURS: return count[3]*4;
-            case FIVES: return count[4]*5;
-            case SIXES: return count[5]*6;
-            case PAIR: return ofKind(2,count);
-            case TWOPAIR: return ofKindTwo(2,2, count);
-            case THREEOFKIND: return ofKind(3,count);
-            case FOUROFKIND: return ofKind(4,count);
-            case FULLHOUSE: return ofKindTwo(2,3, count) > 0 ? 25 : 0;
-            case SMALLSTRAIGHT: return count[0] == 1 && count[1] == 1 && count[2] == 1 && count[3] == 1 && count[4] == 1 ? 30 : 0;
-            case LARGESTRAIGHT: return count[1] == 1 && count[2] == 1 && count[3] == 1 && count[4] == 1 && count[5] == 1 ? 40 : 0;
+            case ONES: return dieCount[1];
+            case TWOS: return dieCount[2]*2;
+            case THREES: return dieCount[3]*3;
+            case FOURS: return dieCount[4]*4;
+            case FIVES: return dieCount[5]*5;
+            case SIXES: return dieCount[6]*6;
+            case PAIR: return findOfKind(2,dieCount);
+            case TWOPAIR: return findTwoOfKind(2,2, dieCount);
+            case THREEOFKIND: return findOfKind(3,dieCount);
+            case FOUROFKIND: return findOfKind(4,dieCount);
+            case FULLHOUSE: return findTwoOfKind(2,3, dieCount) > 0 ? 25 : 0;
+            case SMALLSTRAIGHT: return dieCount[1] == 1 && dieCount[2] == 1 && dieCount[3] == 1 && dieCount[4] == 1 && dieCount[5] == 1 ? 30 : 0;
+            case LARGESTRAIGHT: return dieCount[2] == 1 && dieCount[3] == 1 && dieCount[4] == 1 && dieCount[5] == 1 && dieCount[6] == 1 ? 40 : 0;
             case CHANCE: return rolls[0] + rolls[1] + rolls[2] + rolls[3] + rolls[4];
-            case YAHTZEE: return ofKind(5,count) > 0 ? 50 : 0;
+            case YAHTZEE: return findOfKind(5,dieCount) > 0 ? 50 : 0;
             default:
                 System.err.println("Error: No category was selected");
                 return 0;
@@ -29,23 +29,23 @@ class Yahtzee {
     }
 
     //Method takes in a required amount of same roll die, if nothing is met gives 0.
-    private static int ofKind(int amount, int[] count){
-        for(int i = 5; i >= 0; i--){
-            if(count[i] >= amount){
-                return amount*(i+1);
+    private static int findOfKind(int requiredDice, int[] dieCount){
+        for(int eyes = 6; eyes > 0; eyes--){
+            if(dieCount[eyes] >= requiredDice){
+                return requiredDice*(eyes);
             }
         }
         return 0;
     }
 
     //Method takes in a required amount of same roll die TWICE, if both are not met it gives 0.
-    private static int ofKindTwo(int amountA, int amountB, int[] count){
-        for(int i = 5; i >= 0; i--){
-            if(count[i] >= amountA){
-                for(int j = 5; j >= 0; j--){
-                    if(j == i) continue; //Skip checking the same value
-                    if(count[j] >= amountB){
-                        return (amountA*(i+1)) + (amountB*(j+1));
+    private static int findTwoOfKind(int requiredRollsA, int requiredRollsB, int[] dieCount){
+        for(int eyesA = 6; eyesA > 0; eyesA--){
+            if(dieCount[eyesA] >= requiredRollsA){
+                for(int eyesB = 6; eyesB > 0; eyesB--){
+                    if(eyesB == eyesA) continue; //Skip checking the same value
+                    if(dieCount[eyesB] >= requiredRollsB){
+                        return (requiredRollsA*(eyesA)) + (requiredRollsB*(eyesB));
                     }
                 }
             }
@@ -53,12 +53,12 @@ class Yahtzee {
         return 0;
     }
 
-    //Method takes rolls and counts them, returns amount of select roles in array.
+    //Method takes rolls and counts them, returns amount of select rolls as an array
     private static int[] diceCounter(int[] rolls){
-        int[] counter = new int[]{0,0,0,0,0,0}; //Six possible rolls (Array indexes - 1)
+        int[] dieCount = new int[]{0,0,0,0,0,0,0}; //PS: Index 0 is unused to reduce uncertainty
         for(int dice : rolls){
-            counter[dice-1]++;
+            dieCount[dice]++;
         }
-        return counter;
+        return dieCount;
     }
 }
